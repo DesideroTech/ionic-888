7b668c8c89436239004d06eb62476f23500be312
DesideroTech <desiderotech@gmail.com>
2021-01-19 14:01:59 +0800

    2 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -11,7 +11,7 @@ export class ContactPage {
   height: number;
   weight: number;
   age: number;
-  gender: string;
+  sex: string;
   calories: number;
   physicalactivitylevel: string;
   dci: number;
@@ -19,24 +19,28 @@ export class ContactPage {
   constructor(public navCtrl: NavController) {} 
   calculateDCI() {
 
-  if (this.gender = "m") {
+  if (this.sex = "m") {
    this.calories = (10 * this.weight + 6.25 * this.height - 5 * this.age + 5);
-
-  } else if (this.gender= "f") {
+  } else if (this.sex = "f") {
    this.calories = (10 * this.weight + 6.25 * this.height - 5 * this.age - 161);
+  } else {
+    this.calories=0;
   }
-  if (this.physicalactivitylevel = "1.2") {
+
+  if (this.physicalactivitylevel = "little or no") {
    this.dci = this.calories*1.2;
-  }else if (this.physicalactivitylevel = "1.4") {
+  }else if (this.physicalactivitylevel = "one to two") {
    this.dci = this.calories*1.4;
-  }else if (this.physicalactivitylevel = "1.6") {
+  }else if (this.physicalactivitylevel = "two to three") {
    this.dci = this.calories*1.6;
-  }else if (this.physicalactivitylevel = "1.75") {
+  }else if (this.physicalactivitylevel = "four to five") {
    this.dci = this.calories*1.75;
-  }else if (this.physicalactivitylevel = "2.0") {
+  }else if (this.physicalactivitylevel = "six to seven") {
    this.dci = this.calories*2.0;
-  }else if (this.physicalactivitylevel = "2.3") {
+  }else if (this.physicalactivitylevel = "professional") {
    this.dci = this.calories*2.3;
+  }else{
+    this.dci=0;
   }
   }
   }
