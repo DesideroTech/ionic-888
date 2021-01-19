05fd1984c07d37502d32e73eeafb73cac8065f93
DesideroTech <desiderotech@gmail.com>
2021-01-19 14:45:42 +0800

    3 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -19,25 +19,25 @@ export class ContactPage {
   constructor(public navCtrl: NavController) {} 
   calculateDCI() {
 
-  if (this.sex = "m") {
+  if (this.sex == "m") {
    this.calories = (10 * this.weight + 6.25 * this.height - 5 * this.age + 5);
-  } else if (this.sex = "f") {
+  } else if (this.sex == "f") {
    this.calories = (10 * this.weight + 6.25 * this.height - 5 * this.age - 161);
   } else {
     this.calories=0;
   }
 
-  if (this.physicalactivitylevel = "little or no") {
+  if (this.physicalactivitylevel == "little or no") {
    this.dci = this.calories*1.2;
-  }else if (this.physicalactivitylevel = "one to two") {
+  }else if (this.physicalactivitylevel == "one to two") {
    this.dci = this.calories*1.4;
-  }else if (this.physicalactivitylevel = "two to three") {
+  }else if (this.physicalactivitylevel == "two to three") {
    this.dci = this.calories*1.6;
-  }else if (this.physicalactivitylevel = "four to five") {
+  }else if (this.physicalactivitylevel == "four to five") {
    this.dci = this.calories*1.75;
-  }else if (this.physicalactivitylevel = "six to seven") {
+  }else if (this.physicalactivitylevel == "six to seven") {
    this.dci = this.calories*2.0;
-  }else if (this.physicalactivitylevel = "professional") {
+  }else if (this.physicalactivitylevel == "professional") {
    this.dci = this.calories*2.3;
   }else{
     this.dci=0;
