f927ac80f4a5f850d5e267902d8f588e8fbc2791
DesideroTech <desiderotech@gmail.com>
2021-01-21 08:32:55 +0800

    4 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -12,33 +12,43 @@ export class ContactPage {
   weight: number;
   age: number;
   sex: string;
-  calories: number;
+  caloriesinitial: number;
+  caloriesfinal: number;
   physicalactivitylevel: string;
   dci: number;
+  error: string;
 
   constructor(public navCtrl: NavController) {} 
   calculateDCI() {
 
-  if (this.sex == "m") {
-   this.calories = (10 * this.weight + 6.25 * this.height - 5 * this.age + 5);
-  } else if (this.sex == "f") {
-   this.calories = (10 * this.weight + 6.25 * this.height - 5 * this.age - 161);
+  if (this.age >= 18) {
+   this.caloriesinitial = (10 * this.weight + 6.25 * this.height - 5 * this.age);
+  } else if (this.age >= 18) {
+   this.caloriesinitial = (10 * this.weight + 6.25 * this.height - 5 * this.age);
   } else {
-    this.calories=0;
+    this.error = "This calculator does not provide a valid result for ages 17 and below.";
   }
-
+  
+  if (this.sex == "m") {
+   this.caloriesfinal = (this.caloriesinitial + 5);
+  } else if (this.sex == "f") {
+   this.caloriesfinal = (this.caloriesinitial - 161);
+  } else {
+    this.caloriesfinal=0;
+  }
+  
   if (this.physicalactivitylevel == "little or no") {
-   this.dci = this.calories*1.2;
+   this.dci = this.caloriesfinal*1.2;
   }else if (this.physicalactivitylevel == "one to two") {
-   this.dci = this.calories*1.4;
+   this.dci = this.caloriesfinal*1.4;
   }else if (this.physicalactivitylevel == "two to three") {
-   this.dci = this.calories*1.6;
+   this.dci = this.caloriesfinal*1.6;
   }else if (this.physicalactivitylevel == "four to five") {
-   this.dci = this.calories*1.75;
+   this.dci = this.caloriesfinal*1.75;
   }else if (this.physicalactivitylevel == "six to seven") {
-   this.dci = this.calories*2.0;
+   this.dci = this.caloriesfinal*2.0;
   }else if (this.physicalactivitylevel == "professional") {
-   this.dci = this.calories*2.3;
+   this.dci = this.caloriesfinal*2.3;
   }else{
     this.dci=0;
   }
