c1019e71da19a6c26a1f24e938f8f38c5eaf1a23
DesideroTech <desiderotech@gmail.com>
2021-01-22 02:06:11 +0800

    1 file modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+ 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html',
   styleUrls: ["about.css"]
-
+ 
 })
 export class AboutPage {
-
+ 
   constructor(public navCtrl: NavController) {
-
+ 
   }
-
+ 
 }
+
