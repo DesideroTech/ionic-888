96bd2c94f2ce2cb436d48338079d4f8de512d51c
DesideroTech <desiderotech@gmail.com>
2021-01-22 12:24:30 +0800

    1 file modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -16,7 +16,7 @@ export class ContactPage {
   caloriesfinal: number;
   physicalactivitylevel: string;
   dci: number;
-  error: string;
+  error: ;
  
   constructor(public navCtrl: NavController) {} 
   calculateDCI() {
@@ -26,7 +26,7 @@ export class ContactPage {
   } else if (this.age >= 18 && this.weight > 0 && this.height > 0) {
    this.caloriesinitial = (10 * this.weight + 6.25 * this.height - 5 * this.age);
   } else if (this.weight < 0 && this.height < 0 && this.age < 18) {
-    this.error = "No negative values are allowed. Please refresh and try again. This calculator does not provide a valid result for ages 17 and below.";
+    this.error ="Negative values and ages 17 below are not allowed. Please try again";
   } else if (this.weight < 0 || this.height < 0) {
     this.error = "No negative values are allowed. Please refresh and try again.";
   } else {
